084885db260f0108b197ba97a454837307d00895
SylvesterSmithIII <sylvestermsmithiii@gmail.com>
2023-07-03 21:10:14 -0700

    finished function for showing available moves

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ const COLORS = {
 let board
 let turn
 let winner
+let currentPlayer
+let opponent
+let validMove
+let initialRender
 
 /*----- cached elements  -----*/
-// divs in board
+const boardPieces = [...document.querySelectorAll(`main > div`)]
 const playAgainBtn = document.querySelector('button')
 // display for player turn
 
@@ -23,7 +27,7 @@ init()
 function init() {
     board = [
         [1, 0, 1, 0, 0, 0, -1, 0], // bottom col
-        [0, 1, 0, -1, 0, -1, 0, -1],
+        [0, 1, 0, -1, 0, 0, 0, -1],
         [1, 0, 1, 0, -1, 0, -1, 0],
         [0, 1, 0, -1, 0, -1, 0, -1],
         [1, 0, 1, 0, 0, 0, -1, 0],
@@ -33,11 +37,13 @@ function init() {
     ]
     turn = 1
     winner = null
+    initialRender = false
     render()
 }
 
 function render() {
     renderBoard()
+    renderClickEvents()
     renderMessege()
 }
 
@@ -46,117 +52,192 @@ function renderBoard() {
     board.forEach((colArr, colIdx) => {
         colArr.forEach((elVal, rowIdx) => {
 
-            // Return if value is 0 (empty space)
-            if (elVal === 0) return
-
             // Select that div using its id
             const cellId = `c${colIdx}r${rowIdx}`
-            const cellEl = document.getElementById(cellId)
+            const cellEl = boardPieces.find(el => el.id === cellId)
 
-            // create and style checkers piece div
-            let checkerPiece = document.createElement('div')
-            checkerPiece.style.backgroundColor = COLORS[elVal]
+            // Return if value is 0 (empty space)
+            if (elVal === 0) {
+                cellEl.style.backgroundColor = COLORS[elVal]
+                return
+            }
 
-            // append to that square
-            cellEl.append(checkerPiece)
+
+            if (!initialRender) {
+                // create and style checkers piece div
+                let checkerPiece = document.createElement('div')
+                checkerPiece.style.backgroundColor = COLORS[elVal]
+
+                // append to that square
+                cellEl.append(checkerPiece)
+            }
         })
     })
+    initialRender = true
 }
 
 function renderMessege() {
     console.log()
 }
 
-board.forEach((colArr, colIdx) => {
-    colArr.forEach((elVal, rowIdx) => {
-        if (elVal === turn) {
-            const cellId = `c${colIdx}r${rowIdx}`
-            // add event listners to the checker pieces
-            const cellEl = document.querySelector(`#${cellId} > div`)
-            const firstClick = initialClick(colIdx, rowIdx)
-            cellEl.addEventListener('click', firstClick)
-        }
+function renderClickEvents() {
+    board.forEach((colArr, colIdx) => {
+        colArr.forEach((elVal, rowIdx) => {
+            if (elVal === turn) {
+                const cellId = `c${colIdx}r${rowIdx}`
+                // add event listners to the checker pieces
+                const cellEl = document.querySelector(`#${cellId} > div`)
+                const firstClick = initialClick(colIdx, rowIdx)
+                cellEl.addEventListener('click', firstClick)
+            }
+        })
     })
-})
+}
 
 function initialClick(colIdx, rowIdx) {
     const func = function(event) {
         event.stopPropagation()
-        event.target.removeEventListener('click', func)
+        renderBoard()
+        // event.target.removeEventListener('click', func)
         let moves = possibleMoves(colIdx, rowIdx)
-        event.target.addEventListener('click', chooseMove)
+        console.log(moves)
+        showPossibleMoves(moves)
+        // event.target.addEventListener('click', chooseMove)
     }
     return func
 }
 
-function chooseMove(event) {
-    event.stopPropagation();
-    console.log("second click")
-    event.target.removeEventListener('click', chooseMove)
+// function chooseMove(event) {
+//     event.stopPropagation();
+//     console.log("second click")
+//     event.target.removeEventListener('click', chooseMove)
 
-    event.target.addEventListener('click', initialClick)
-}
+//     event.target.addEventListener('click', initialClick)
+// }
 
 function possibleMoves(colIdx, rowIdx) {
-    const currentPlayer = board[colIdx][rowIdx]
-    let moves = {}
-  
-    if (turn === 1) {
+    currentPlayer = board[colIdx][rowIdx]
+    opponent = findOpponent(currentPlayer)
+    let moves, topLeft, topRight, bottomLeft, bottomRight  = {}
+    
+    if (currentPlayer === 1 || currentPlayer === 2 || currentPlayer === -2) {
       // Top left move
-      const topLeftCol = colIdx - 1
-      const topLeftRow = rowIdx + 1
-      if (isValidPosition(topLeftCol, topLeftRow)) {
-        moves.leftMove = board[topLeftCol][topLeftRow]
-        moves.leftPositionId = `c${topLeftCol}r${topLeftRow}`
-        console.log(moves)
-        moves = checkForOvertake(currentPlayer, moves)
-        console.log(moves)
-      }
-  
+        validMove = true
+        topLeft = checkTopLeft(colIdx, rowIdx)
       // Top right move
-      const topRightCol = colIdx + 1
-      const topRightRow = rowIdx + 1
-      if (isValidPosition(topRightCol, topRightRow)) {
-        moves.rightMove = board[topRightCol][topRightRow]
-        moves.rightPositionId = `c${topRightCol}r${topRightRow}`
-        moves = checkForOvertake(currentPlayer, moves)
-      }
-    } else if (turn === -1) {
+      validMove = true
+        topRight = checkTopRight(colIdx, rowIdx)
+    } else if (currentPlayer === -1 || currentPlayer === 2 || currentPlayer === -2) {
       // Bottom left move
-      const bottomLeftCol = colIdx - 1
-      const bottomLeftRow = rowIdx - 1
-      if (isValidPosition(bottomLeftCol, bottomLeftRow)) {
-        moves.leftMove = board[bottomLeftCol][bottomLeftRow]
-        moves.leftPositionId = `c${bottomLeftCol}r${bottomLeftRow}`
-      }
-  
+        validMove = true
+        bottomLeft = checkBottomLeft(colIdx, rowIdx)
       // Bottom right move
-      const bottomRightCol = colIdx + 1
-      const bottomRightRow = rowIdx - 1
-      if (isValidPosition(bottomRightCol, bottomRightRow)) {
-        moves.rightMove = board[bottomRightCol][bottomRightRow]
-        moves.rightPositionId = `c${bottomRightCol}r${bottomRightRow}`
-      }
+        validMove = true
+        bottomRight = checkBottomRight(colIdx, rowIdx)
     }
   
-    return moves;
+    moves = [{...topLeft}, {...topRight}, {...bottomLeft}, {...bottomRight}]
+    // trim empty objects from array of objects
+    const filteredList = moves.filter((obj) => Object.keys(obj).length !== 0);
+    return filteredList
   }
+
+function checkTopLeft(colIdx, rowIdx) {
+    let moves = {}
+    const topLeftCol = colIdx - 1
+    const topLeftRow = rowIdx + 1
+    if (isValidPosition(topLeftCol, topLeftRow)) {
+        let move = board[topLeftCol][topLeftRow]
+        if (move === 0) {
+            moves.leftMove = move
+            moves.moveId = `c${topLeftCol}r${topLeftRow}`
+            return moves
+        } else if (opponent.includes(move) && validMove) {
+            validMove = false
+            moves = checkTopLeft(topLeftCol, topLeftRow)
+        }
+    }
+    return moves
+}
+
+function checkTopRight(colIdx, rowIdx) {
+    let moves = {}
+    const topRightCol = colIdx + 1
+    const topRightRow = rowIdx + 1
+    if (isValidPosition(topRightCol, topRightRow)) {
+        let move = board[topRightCol][topRightRow]
+        if (move === 0) {
+            moves.rightMove = move
+            moves.moveId = `c${topRightCol}r${topRightRow}`
+            return moves
+        } else if (opponent.includes(move) && validMove) {
+            validMove = false
+            moves = checkTopRight(topRightCol, topRightRow)
+        }
+    }
+    return moves
+}
+
+function checkBottomLeft(colIdx, rowIdx) {
+    let moves = {}
+    const bottomLeftCol = colIdx - 1
+    const bottomLeftRow = rowIdx - 1
+    if (isValidPosition(bottomLeftCol, bottomLeftRow)) {
+        let move = board[topLeftCol][topLeftRow]
+        if (move === 0) {
+            moves.leftMove = move
+            moves.moveId = `c${bottomLeftCol}r${bottomLeftRow}`
+            return moves
+        } else if (opponent.includes(move) && validMove) {
+            validMove = false
+            moves = checkBottomLeft(bottomLeftCol, bottomLeftRow)
+        }
+    }
+    return moves
+}
+
+function checkBottomRight(colIdx, rowIdx) {
+    let moves = {}
+    const bottomRightCol = colIdx + 1
+    const bottomRightRow = rowIdx - 1
+    if (isValidPosition(bottomRightCol, bottomRightRow)) {
+        let move = board[topLeftCol][topLeftRow]
+        if (move === 0) {
+            moves.rightMove = move
+            moves.moveId = `c${bottomRightCol}r${bottomRightRow}`
+            return moves
+        } else if (opponent.includes(move) && validMove) {
+            validMove = false
+            moves = checkBottomRight(bottomRightCol, bottomRightRow)
+        }
+    }
+    return moves
+}
 
 function isValidPosition(col, row) {
     return col >= 0 && col < board.length && row >= 0 && row < board[col].length
   }
 
-function checkForOvertake(player, moves) {
-    let opponent = turn * -1
-    if (moves["leftMove"] === opponent) {
-        let col = parseInt(moves.leftPositionId.slice(1, 2)) - 1
-        let row = parseInt(moves.leftPositionId.slice(3, 4)) + 1
-        if (isValidPosition(col, row)) {
-            if (board[col][row] === 0) {
-                moves.leftMove = board[col][row]
-                moves.leftPositionId = `c${col}r${row}`
-            }
-        }
-        return moves
+function findOpponent(player) {
+    opponent = []
+    if (player > 0) {
+        return opponent = [-1, -2]
+    } else {
+        return opponent = [1, 2]
     }
-  }
+}
+
+function showPossibleMoves(possibleMoves) {
+
+    possibleMoves.forEach(moves => {
+        const divId = moves["moveId"]
+        console.log(divId)
+        let availableMoveEl = boardPieces.find(el => el.id === divId)
+        availableMoveEl.style.backgroundColor = "gray"
+        availableMoveEl.addEventListener('click', finalGuess)
+    })
+}
+
+function finalGuess() {
+    
+}
